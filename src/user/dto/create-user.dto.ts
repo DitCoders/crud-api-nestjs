ba7bf450c637b455b03dae817bb9d307051f4615
DitCoders <ditcoders@gmail.com>
2023-08-04 05:16:49 +0700

    penambahan validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   readonly nama: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly alamat: string;
 }
