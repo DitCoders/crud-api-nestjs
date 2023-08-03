6a63b83f6b3087b152f10a85f9b77ea7b8271967
DitCoders <ditcoders@gmail.com>
2023-08-04 05:37:30 +0700

    penambahan api docs yaitu swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("crudApi")
+    .setDescription("Simple CRUD with NestJS, TypeORM, and MySQL")
+    .setVersion("1.0")
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("docs", app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly nama: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly alamat: string;
 }
